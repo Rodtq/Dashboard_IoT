9d24f0a64583dd9968d191066ab9fc62e9ec5e8b
Rodrigo Tavernaro Queiroz <rodtq@hotmail.com>
2022-06-22 13:51:05 -0300

    breadcrumb initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
 import './App.css';
-import Hamburguer from './components/Hamburger';
-import ActionMenu from './components/ActionMenu';
-import AddPage from './pages/AddPage';
-import DelPage from './pages/DelPage';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import { Breadcrumbs, Link, Typography } from '@mui/material';
-import Home from './pages/Home';
+
+import Navigator from './components/Navigator';
+
+
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="/">
-            MUI
-          </Link>
-          <Link
-            underline="hover"
-            color="inherit"
-            href="/material-ui/getting-started/installation/">
-            Core
-          </Link>
-          <Typography color="text.primary">Breadcrumbs</Typography>
-        </Breadcrumbs>
+ 
       </header>
       <body>
-        <Hamburguer></Hamburguer>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home/>} />
-            <Route path="addPage" element={<AddPage ></AddPage>}/>
-            <Route path="delPage" element={<DelPage></DelPage>} />
-          </Routes>
-        </BrowserRouter>
-        <ActionMenu></ActionMenu>
+        <Navigator>
+        </Navigator>
       </body>
     </div>
   );
